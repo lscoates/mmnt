7325dc4a3500102b811b482f2338a339de7ec328
Luke Scoates <lscoates@gmail.com>
2020-12-20 10:16:52 -0800

    Add helper methods for rendering React components

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def react_component(component, props = {}, tag = 'div')
+    id = "react-#{component.underscore.dasherize}"
+    data = { camelized_props: ReactHelper.camelize_props(props.as_json) }
+
+    content_tag tag, id: id, data: data do
+      javascript_pack_tag component
+    end
+  end
 end
