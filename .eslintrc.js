edfcef0fe4acee813aac433482cebf0cb5c513e9
Luke Scoates <lscoates@gmail.com>
2020-12-20 22:01:28 -0800

    Adds shared form components

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  extends: 'airbnb',
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'import',
+  ],
+  env: {
+    browser: true,
+    node: true,
+  },
 };
---
+++ app/javascript/components/shared/Input.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Input = (props) => {
+  const {
+    id,
+    isDisabled,
+    hasError,
+    helpText,
+    label,
+    name,
+    onChange,
+    placeholder,
+    value,
+  } = props;
+
+  return (
+    <div className="form-group">
+      <label htmlFor={id}>
+        {label}
+      </label>
+      <input
+        id={id}
+        name={name}
+        type="text"
+        className="form-control"
+        value={value}
+        placeholder={placeholder}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      {helpText && (
+        <small className="form-text text-muted">
+          {helpText}
+        </small>
+      )}
+    </div>
+  );
+};
+
+Input.defaultProps = {
+  id: '',
+  isDisabled: false,
+  hasError: false,
+  helpText: null,
+  label: '',
+  placeholder: '',
+  value: '',
+};
+
+Input.propTypes = {
+  id: PropTypes.string,
+  isDisabled: PropTypes.bool,
+  hasError: PropTypes.bool,
+  helpText: PropTypes.string,
+  label: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+export default Input;
